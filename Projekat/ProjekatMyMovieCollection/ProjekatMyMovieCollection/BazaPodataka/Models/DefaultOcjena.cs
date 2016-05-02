e35f73d4fc15811dabf6c78aa617f2d176edaca5
Amra Mujcinovic <amujcinovi3@etf.unsa.ba>
2016-05-02 22:45:31 +0200

    izmjene klasa

---
+++ Projekat/ProjekatMyMovieCollection/ProjekatMyMovieCollection/BazaPodataka/Models/DefaultOcjena.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatMyMovieCollection.BazaPodataka.Models
+{
+    class DefaultOcjena
+    {
+
+        public static void Initialize(OcjenaDbContext context)
+        {
+
+            if (!context.Ocjene.Any())
+            {
+                context.Ocjene.AddRange(
+                    new Ocjena()
+                    {
+                        opis = "extra",
+                        ocjena = 5
+                    }
+
+                );
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Projekat/ProjekatMyMovieCollection/ProjekatMyMovieCollection/BazaPodataka/Models/FilmDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatMyMovieCollection.BazaPodataka.Models
+{
+    class FilmDbContext
+    {
+      
+        public DbSet<Film> Filmovi { get; set; }
+       
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string databaseFilePath = "Ooadbaza.db";
+            try
+            {
+
+                databaseFilePath = Path.Combine(ApplicationData.Current.LocalFolder.Path, databaseFilePath);
+            }
+            catch (InvalidOperationException) { }
+
+            optionsBuilder.UseSqlite($"Data source={databaseFilePath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.Entity<Korisnik>().Property(p => p.slika).HasColumnType("image");
+        }
+    }
+
+}
