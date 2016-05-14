086993c497cf11f46783994c4c802c3611594b26
Berina Muhović <bmuhovic1@etf.unsa.ba>
2016-05-15 00:34:20 +0200

    Formice..

---
+++ Projekat/ProjekatMyMovieCollection/ProjekatMyMovieCollection/BazaPodataka/Views/KolekcijaViewModel.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ProjekatMyMovieCollection.BazaPodataka.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class KolekcijaListViewxaml : Page
+    {
+        public KolekcijaListViewxaml()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button_Click(System.Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(System.Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textBlock_SelectionChanged(System.Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textBlock_SelectionChanged(System.Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textBlock_SelectionChanged(System.Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textBlock2_SelectionChanged(System.Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textBlock2_SelectionChanged(System.Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void listView_SelectionChanged(System.Object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void textBlock3_SelectionChanged(System.Object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
