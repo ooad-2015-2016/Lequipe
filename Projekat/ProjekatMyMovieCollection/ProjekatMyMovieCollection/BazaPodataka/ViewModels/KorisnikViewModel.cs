4ba513a45f62a29f06edc13d5e305643c1616bf7
Amra Mujcinovic <amujcinovi3@etf.unsa.ba>
2016-05-17 01:36:17 +0200

    klase doradjene

--- Projekat/ProjekatMyMovieCollection/ProjekatMyMovieCollection/BazaPodataka/ViewModels/KorisnikViewModel.cs
+++ Projekat/ProjekatMyMovieCollection/ProjekatMyMovieCollection/BazaPodataka/ViewModels/KorisnikViewModel.cs
@@ -11,5 +11,30 @@ namespace ProjekatMyMovieCollection.BazaPodataka.ViewModels
         //pocetna iz nje se nasljedjuju ostale i ona je Parent 
 
         public Korisnik Korisnik { get; set; }
+
+        public bool JaSamAdmin { get; set; } //potrebno kad zeli neko ObrisatiKorisnika, to nije moguce ako osoba nije admin ;)
+
+        public ICommand RegistrujKorisnika { get; set; }
+        public ICommand LoginKorisnika { get; set; }
+        public ICommand LogoutKorisnika { get; set; } 
+        public ICommand ObrisiKorisnika { get; set; }
+        //... jos dodat PregledSvihKorisnika
+        
+
+        
+
+        public KorisnikViewModel()
+        {
+            
+          
+            RegistrujKorisnika = new RelayCommand<object>(registrujKorisnika, true);
+            LoginKorisnika = new RelayCommand<object>(loginKorisnika, mozeLiSeLogoutKorisnik);
+            LoginKorisnika = new RelayCommand<object>(logoutKorisnika, mozeLiSeLoginKorisnik);
+            ObrisiKorisnika = new RelayCommand<object>(obrisiKorisnika, mozeLiSeObrisatiKorisnik);
+
+            //mozda treba i sacuvaj izmjene u kolekciji hm... nakon dodavanja filma
+            //IMA SMISLA TO ... BYBERNS
+            SacuvajIzmjene = new RelayCommand<object>(sacuvajIzmjene, zeliteLiSacuvatiIzmjene);
+        }
     }
 }
